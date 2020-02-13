a1d312ec64fd18a90e442c2848eb9a158e21b617
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-02-13 13:19:08 +0530

    Implementation of SUT

---
+++ src/test/java/com/in28minutes/junit/mockito/FirstMockitoTest.java
@@ -0,0 +1,21 @@
+package com.in28minutes.junit.mockito;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/*
+ * TodoService.java => External Interface for this application
+ * 
+ * ToDoBusinessImpl.java => retrieve todos related to Spring
+ * 
+ * */
+
+public class FirstMockitoTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);   
+	}
+
+}
