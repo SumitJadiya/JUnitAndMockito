1a91d4a2f43ef6aad7c799dc4b4814bcbc0e5070
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-02-13 15:41:11 +0530

    Test Cases using Stub

---
+++ src/test/java/com/in28minutes/data/api/TodoServiceStub.java
@@ -0,0 +1,14 @@
+package com.in28minutes.data.api;
+
+import java.util.Arrays;
+import java.util.List;
+
+// Dummy implementation of TodoService
+public class TodoServiceStub implements TodoService {
+
+	public List<String> retrieveTodos(String user) {
+
+		return Arrays.asList("Learn Spring", "Learn JPA", "Learn Spring Boot");
+	}
+
+}
