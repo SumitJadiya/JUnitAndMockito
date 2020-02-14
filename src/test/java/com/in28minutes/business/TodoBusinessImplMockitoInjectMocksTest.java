e2f47b42a605019a31d5dba601244cb371342eb7
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-02-14 08:07:28 +0530

    Using MockitoJUnit.rule() instead of @RunWith(MockitoJUnitRunner.class)

--- src/test/java/com/in28minutes/business/TodoBusinessImplMockitoInjectMocksTest.java
+++ src/test/java/com/in28minutes/business/TodoBusinessImplMockitoInjectMocksTest.java
@@ -14,19 +14,22 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import com.in28minutes.data.api.TodoService;
 
-@RunWith(MockitoJUnitRunner.class)
 public class TodoBusinessImplMockitoInjectMocksTest {
 
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
+	
 	@Mock
 	TodoService serviceMock;
 
